2fe79e7a7ed4fa37758f80084ec21dcccc50e95b
franckchen <franckchen@foxmail.com>
2016-06-19 06:58:02 +0000

    update

--- FE/AMD/lib/mxy.js
+++ FE/AMD/lib/mxy.js
@@ -1,4 +1,5 @@
 define(function (require) {
+    // return 用于暴露方法给引用方使用，可以是任何类型
     return {
         name: 'maxinyu'
     };
--- FE/AMD/main.js
+++ FE/AMD/main.js
@@ -1,8 +1,8 @@
 define(function (require) {
     var cf = require('cf');
     var mxy = require('mxy');
-    // require('newCF');
 
+    // 被require时 ，下面这些代码是会被运行的
     console.log(cf.name);
     console.log(mxy.name);
 });
